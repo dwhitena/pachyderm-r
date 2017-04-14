5504c4ed226c7e4a775033371384d2e50008062e
Josiah Baker <josiah.baker@forrent.com>
2017-04-14 13:50:48 -0400

    Modify styling in R code
    change indentation and passing function arguments to adhere to R style guides

--- pachr/infer.R
+++ pachr/infer.R
@@ -1,16 +1,25 @@
-library(caret)
-
 # load model
 load("model.rda")
 
 # loop over input files performing inference
-files <- list.files(path="/pfs/attributes", pattern="*.csv", full.names=T, recursive=FALSE)
+files <- list.files(path = "/pfs/attributes", 
+					pattern = "*.csv", 
+					full.names = T, 
+					recursive = FALSE)
+					
+cols <- c("Sepal.Length",
+		  "Sepal.Width",
+		  "Petal.Length",
+		  "Petal.Width")
+		  
 for (file in files){
 	# load the data
 	dataset <- read.csv(file, header=FALSE)
-        colnames(dataset) <- c("Sepal.Length","Sepal.Width","Petal.Length","Petal.Width")
-        # perform the inference
+        colnames(dataset) <- cols
+        
+		# perform the inference
         predictions <- predict(fit.svm, dataset)
-        # output the results
-        write.csv(predictions, file=paste("/pfs/out/", file, sep="")) 
+        
+		# output the results
+        write.csv(predictions, file = paste("/pfs/out/", file, sep = "")) 
 }
